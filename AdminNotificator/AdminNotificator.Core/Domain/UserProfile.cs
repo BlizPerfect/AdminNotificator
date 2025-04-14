ee55d9fa7cf728d68b967b93e6668e429713ed77
Nikita Shevyrin <nikita.shevyrin@gmail.com>
2025-04-14 12:59:27 +0500

    Undid userprofile constructor

--- AdminNotificator/AdminNotificator.Core/Domain/UserProfile.cs
+++ AdminNotificator/AdminNotificator.Core/Domain/UserProfile.cs
@@ -92,10 +92,6 @@ public class UserProfile
     
     protected UserProfile() { }
 
-    public UserProfile()
-    {
-    }
-
     private UserProfile(string login, string sid, string firstname, string surname, string patronymic,
         string email, DateTime? seniorityDate, int? suspendedSeniorityDays, IEnumerable<UserDepartment> departmentItems,
         IEnumerable<UserPosition> positions, int[] departmentIds, UserStatus userStatus, UserOffice userOffice,
