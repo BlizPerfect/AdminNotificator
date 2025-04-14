ced97504a18e92dc7e565768a3060b9b20fb6501
Zakhar <zax_2000@mail.ru>
2025-04-14 15:05:05 +0500

    Доудаление папок obj

--- AdminNotificator/AdminNotificator.Tests/WebApiTests.cs
+++ AdminNotificator/AdminNotificator.Tests/WebApiTests.cs
@@ -1,5 +1,9 @@
+using System;
+using System.Collections.Generic;
 using System.Net;
+using System.Net.Http;
 using System.Net.Http.Json;
+using System.Threading.Tasks;
 using AdminNotificator.Core;
 using AdminNotificator.Core.Domain;
 using Microsoft.AspNetCore.Mvc.Testing;
